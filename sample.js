f144e15104c2ae3fb8bfd716f8762f92a8d5a2ce
iain <iain.coulthard@gmail.com>
2016-09-21 21:05:29 +1000

    added dir blocks

--- sample.js
+++ sample.js
@@ -12,12 +12,29 @@
        console.log('In my_first_block');
     };
 
+    ext.robot_forward = function() {
+       console.log('In robot_forward');
+    };
+    ext.robot_back = function() {
+       console.log('In robot_back');
+    };
+    ext.robot_left = function() {
+       console.log('In robot_left');
+    };
+    ext.robot_right = function() {
+       console.log('In robot_right');
+    };
+
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
 	    // Block type, block name, function name
 	    [ ' ', 'my first block', 'my_first_block' ],
+	    [ ' ', 'robot forwards', 'robot_forwards' ],
+	    [ ' ', 'robot back', 'robot_back' ],
+	    [ ' ', 'robot left', 'robot_left' ],
+	    [ ' ', 'robot right', 'robot_right' ],
         ]
     };
 
